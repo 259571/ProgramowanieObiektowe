7181ff4a399467be1d120e861ea5df311b334243
259571 <259571@student.pwr.edu.pl>
2021-03-22 23:22:59 +0100

    1.4

--- tablica.cpp
+++ tablica.cpp
@@ -4,22 +4,27 @@ using namespace std;
 
 int main(void)
 {
-	
+	// pobranie wymiarow tablicy 
 	int x, y;
-	cout << "Liczba wierszy: ";
+	cout << "Podaj liczbe wierszy: ";
 	cin >> x;
-	cout << "Liczba kolumn: ";
+	cout << "Podaj liczbe kolumn: ";
 	cin >> y;
 
-
+	// zajmujje miejsce w pamieci na tablice 
 	double** tab = new double* [x];
 	for (int i = 0; i < x; ++i)
 	{
+	// uzytkownik wypelnia tablice 
 		tab[i] = new double[y];
 		for (int j = 0; j < y; ++j) 
-			tab[i][j] = (i + j) ;
+			{
+				cout << "Podaj wartosc komorki: "  << i << ", " << j << ": ";
+				cin >> tab[i][j];
+			}
+			
 	}
-
+	// wyswietlam tablice
 	for (int i = 0; i < x; ++i, cout << endl)
 		for (int j = 0; j < y; ++j)
 			cout << tab[i][j] << '\t';
