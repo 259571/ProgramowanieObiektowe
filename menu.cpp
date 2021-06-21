c07f26446f27be13577f44b24fcc95c66483456c
259571 <259571@student.pwr.edu.pl>
2021-06-21 19:27:46 +0200

    Koncowy program

--- menu.cpp
+++ menu.cpp
@@ -2,75 +2,127 @@
 #include "tablica_wysw.h"
 #include "menu.h"
 #include "tablica.h"
+#include "pliki.h"
 #include <cstdlib>
 
 using namespace std;
 
 int opcja;
 
+
 void menu()
 {
+	
+		cout << "\t Witaj uzytkowniku! " << endl << endl;
+		cout << "Widzisz przed soba program ktory tworzy arkusz kalkulacyjny." << endl << endl;
+		cout << "Mozesz w nim tworzyc i modyfikowac wymiary tablicy" << endl; 
+		cout << "oraz przeprowadzic proste działania matematyczne" << endl << endl;
+		cout << "Teraz stworz swoja tablice! " << endl << endl;
 
-	int x, y;
-
-	double **arkusz = tworz_arkusz(&x, &y);
+	Tablica arkusz = tworz_arkusz();
 
 	for (;;)
 	{
 
 		cout << "\t MENU" << endl;
 		cout << "_______________________" << endl;
-		cout << "Wybierz opcje z menu" << endl
-			 << endl;
+		cout << "Wybierz opcje z menu" << endl << endl;	 
 		cout << "1. Zmiana rozmiaru tablicy " << endl;
 		cout << "2. Zmiana zawartosci tablicy " << endl;
-		cout << "3. Dodawanie zawartosci komorek" << endl;
-		cout << "4. Odejmowanie zawartosci komorek" << endl;
-		cout << "5. Mnozenie zawartosci komorek" << endl;
-		cout << "6. Dzielenie zawartosci komorek" << endl;
-		cout << "7. Wyscie z programu " << endl;
+		cout << "3. Wyswietlenie tablicy" << endl;
+		cout << "4. Dodawanie zawartosci komorek" << endl;
+		cout << "5. Odejmowanie zawartosci komorek" << endl;
+		cout << "6. Mnozenie zawartosci komorek" << endl;
+		cout << "7. Dzielenie zawartosci komorek" << endl;
+		cout << "8. Sumowanie zawartosci wedlug kolumn" << endl;
+		cout << "9. Sumowanie zawartosci wedlug wierszy" << endl;
+		cout << "10. Zapis tablicy do pliku" << endl;
+		cout << "11. Wczytanie tablicy z pliku" << endl;
+		cout << "12. Wyscie z programu " << endl;
 
-		cin >> opcja;
+		opcja = wprowadz();
+
+		system("clear");
 
 		switch (opcja)
 		{
 		case 1:
 		{
-			void zmiana_rozmiaru (char znak);
+
+			int nwiersze, nkolumny;
+			
+			cout << "1. Wybrales opcje zmiany rozmiaru tablicy" << endl;
+
+			cout << "Podaj nowa ilosc wierszy: " << endl;
+			cin >> nwiersze;
+			cout << "Podaj nowa ilosc kolumn: " << endl;
+			cin >> nkolumny;
+	
+
+			arkusz.zmiana_rozmiaru_tablicy (nwiersze, nkolumny);
 		}
 		break;
 
 		case 2:
 		{
-			void zmiana_elementu (char znak, int x, int y, double** tablica);
+			zmiana_elementu (&arkusz);
 		}
 		break;
 
 		case 3:
 		{
-			void suma_kom (char znak, int x, int y, int i, int j, float wynik, double** tablica);
+			tablica_wyswietlanie (arkusz);
 		}
 		break;
 
 		case 4:
 		{
-			void odej_kom (char znak, int x, int y, int i, int j, float wynik, double** tablica);
+			 suma_kom (&arkusz);
 		}
 		break;
 
 		case 5:
 		{
-			void mno_kom (char znak, int x, int y, int i, int j, float wynik, double** tablica);
+			odej_kom (&arkusz);
 		}
 		break;
 
 		case 6:
 		{
-			void dziel_kom (char znak, int x, int y, int i, int j, float wynik, double** tablica);
+			mno_kom (&arkusz);
 		}
 		break;
 
 		case 7:
+		{
+			dziel_kom (&arkusz);
+		}
+		break;
+
+		case 8:
+		{
+			sum_wg_kolumn (&arkusz);
+		}
+		break;
+
+		case 9:
+		{
+			sum_wg_wierszy (&arkusz);
+		}
+	
+		case 10:
+		{
+			zapis_danych (&arkusz);
+		}
+		break;
+
+		case 11:
+		{
+			wczyt_danych (&arkusz);
+		}
+		break;
+
+		case 12:
 		{
 			exit(0);
 		}
@@ -82,18 +134,7 @@ void menu()
 		}
 		}
 
-		system("clear");
+		
 	}
 }
 
-double **tworz_arkusz(int *x, int *y)
-{
-	// pobranie wymiarow tablicy
-
-	cout << "Podaj liczbe wierszy: ";
-	cin >> *x;
-	cout << "Podaj liczbe kolumn: ";
-	cin >> *y;
-
-	return tworz_tablice(*x, *y);
-}
